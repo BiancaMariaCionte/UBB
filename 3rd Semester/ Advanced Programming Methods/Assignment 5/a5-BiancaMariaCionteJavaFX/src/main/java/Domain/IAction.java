178523cc49a45831559762c0e48c536b6e14bc28
BiancaMariaCionte <147051212+BiancaMariaCionte@users.noreply.github.com>
2024-03-04 18:47:02 +0200

    Delete 3rd Semester/ Advanced Programming Methods/Assignment 5/a5-BiancaMariaCionteJavaFX directory

--- 3rd Semester/ Advanced Programming Methods/Assignment 5/a5-BiancaMariaCionteJavaFX/src/main/java/Domain/IAction.java
+++
@@ -1,7 +0,0 @@
-package Domain;
-
-public interface IAction<ID,T> {
-    void executeUndo() throws ValidationException;
-
-    void executeRedo() throws ValidationException;
-}
--- 3rd Semester/ Advanced Programming Methods/Assignment 5/a5-BiancaMariaCionteJavaFX/src/main/java/Repository/PatientRepository.java
+++
@@ -1,6 +0,0 @@
-package Repository;
-
-import Domain.Patient;
-
-public class PatientRepository extends MemoryRepository<Integer, Patient> {
-}
